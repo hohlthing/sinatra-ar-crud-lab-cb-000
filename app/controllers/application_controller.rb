b0e6ab4175709a0e8651f8e4f2b0b9a48631e364
michael hohl <mhohl@rocketmail.com>
2018-05-21 18:09:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,31 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
-  end
   get '/posts/new' do
     erb :new
   end
+
   post '/posts' do
     @post = Post.create(params)
     erb :index
   end
+
   get '/posts' do
     @posts = Post.all
     erb :index
   end
-  get '/posts/:id' do
-    @post =  Post.find_by_id(params[:id])
+
+  get 'posts/:id' do
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
-  get '/posts/:id/edit' do
+
+  get 'posts/:id/edit' do
     @post = Post.find_by_id(params[:id])
-   erb :edit
+    erb :edit
   end
-  patch '/posts/:id' do  #updates a post
+
+  patch '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
@@ -38,8 +40,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-
-  delete '/posts/:id/delete' do #delete action
+  delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :deleted
